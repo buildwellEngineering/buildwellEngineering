d60322abcf1c9ef483c88faa7756357760466097
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-07-27 17:41:27 +0530

    sixth

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://build-well-front-end.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     allowedHeaders: ['Content-Type'], // Allowed headers
     credentials: true // If you need credentials

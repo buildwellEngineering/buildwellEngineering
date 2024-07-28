e91876a7ab2d0c7363daf4503e6c7aac4f734d6c
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-07-28 20:03:01 +0530

    8th

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -8,8 +8,8 @@ const connectionString = process.env.MONGO_URL;
 
 export const con=async()=>{
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Construct');
-        // await mongoose.connect(connectionString);
+        // await mongoose.connect('mongodb://127.0.0.1:27017/Construct');
+        await mongoose.connect(connectionString);
         console.log("DATABASE CONNECTED SUCCESSFULLY !!!");
     } catch (error) {
         console.log("DATABASE CONNECTION FAILED !!!",error);

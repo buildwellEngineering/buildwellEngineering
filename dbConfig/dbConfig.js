44cc906b570f034b1647092d982950f75d6dc757
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-08-13 16:47:46 +0530

    fourtheenth

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -1,9 +1,10 @@
+
 import mongoose from "mongoose";
 
 export const con=async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URL);
-        // console.log("DATABASE CONNECTED SUCCESSFULLY !!!");
+        console.log("ss")
     } catch (error) {
         console.log("DATABASE CONNECTION FAILED !!!",error);
     }
